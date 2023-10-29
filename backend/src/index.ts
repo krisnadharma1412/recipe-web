1662e6268632b7246a79a5a3d3c5e756fca31242
krisnadharma1412 <iputukrisnadharma@gmail.com>
2023-10-29 10:54:40 +0700

    Setup Backend, Setup Database and Prisma, Setup Spoonacular API, Create Search Endpoint

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import * as RecipeAPI from './recipe-api'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/recipes/search", async (req, res)=> {
+    // res.json({message: 'success!'})
+
+    // GET http://localhost/api/recipes/search?searchTerm=burger&page=1
+    const searchTerm = req.query.searchTerm as string;
+    const page = parseInt(req.query.page as string);
+    const results = await RecipeAPI.searchRecipes(searchTerm, page);
+    
+    return res.json(results);
+})
+
+app.listen(5000, () => {
+    console.log("server running on localhost:5000");
+})
