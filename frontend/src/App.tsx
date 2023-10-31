08320de01211d7e99e66616e61a2db6a02f95bcf
krisnadharma1412 <iputukrisnadharma@gmail.com>
2023-10-31 22:09:55 +0700

    Display search results on UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,36 @@
-import './App.css';
+import "./App.css";
+import { FormEvent, useState } from "react";
+import * as api from "./api";
+import { Recipe } from "./types";
 
 const App = () => {
-  return <div>Hello from recipe App!</div>
-}
+  const [searchTerm, setSearchTerm] = useState("burgers");
+  const [recipes, setRecipes] = useState<Recipe[]>([])
+
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1)
+      setRecipes(recipes.results);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={(event)=> handleSearchSubmit(event)}>
+        <button type='submit'>Submit</button>
+      </form>
+      {recipes.map((recipe) => (
+        <div>
+          recipe image location: {recipe.image}
+          recipe title: {recipe.title}
+        </div>
+      ))}
+    </div>
+  );
+
+};
 
 export default App;
