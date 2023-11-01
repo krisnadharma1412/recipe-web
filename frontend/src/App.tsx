95717ca9642119b8bbc1d42a9d9e527c4266ccde
krisnadharma1412 <iputukrisnadharma@gmail.com>
2023-11-01 10:34:31 +0700

    Add view more button and pagination

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import * as api from "./api";
 import { Recipe } from "./types";
 import RecipeCard from "./components/RecipeCard";
@@ -7,16 +7,29 @@ import RecipeCard from "./components/RecipeCard";
 const App = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [recipes, setRecipes] = useState<Recipe[]>([])
+  const pageNumber = useRef(1);
 
   const handleSearchSubmit = async (event: FormEvent) => {
     event.preventDefault();
     try {
       const recipes = await api.searchRecipes(searchTerm, 1)
       setRecipes(recipes.results);
+      pageNumber.current = 1
     } catch (e) {
       console.log(e);
     }
   };
+
+  const handleViewMoreClick = async () => {
+    const nextPage = pageNumber.current + 1;
+    try {
+      const nextRecipes = await api.searchRecipes(searchTerm, nextPage)
+      setRecipes([...recipes, ...nextRecipes.results]);
+      pageNumber.current = nextPage;
+    } catch (error){
+      console.log(error);
+    }
+  }
 
   return (
     <div>
@@ -33,6 +46,8 @@ const App = () => {
       {recipes.map((recipe) => (
         <RecipeCard recipe={recipe}/>
       ))}
+
+      <button className="view-more-button" onClick={handleViewMoreClick}>View More</button>
     </div>
   );
 
