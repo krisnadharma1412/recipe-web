548613366d7454d70d55eb0006897abb43c99b5e
krisnadharma1412 <iputukrisnadharma@gmail.com>
2023-11-02 11:48:09 +0700

    Add Favourite Functionality to the UI

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -1,19 +1,39 @@
-import { Recipe } from "../types"
+import { Recipe } from "../types";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 interface Props {
-    recipe: Recipe
-    onClick: ()=> void;
+  recipe: Recipe;
+  isFavourite: boolean;
+  onClick: () => void;
+  onFavouriteButtonClick: (recipe: Recipe) => void;
 }
 
-const  RecipeCard = ({ recipe, onClick }: Props) => {
-    return (
-        <div className="recipe-card" onClick={onClick}>
-            <img src={recipe.image} alt="recipe.title"></img>
-            <div className="recipe-card-title">
-                <h3>{recipe.title}</h3>
-            </div>
-        </div>
-    );
-};   
+const RecipeCard = ({
+  recipe,
+  onClick,
+  onFavouriteButtonClick,
+  isFavourite,
+}: Props) => {
+  return (
+    <div className="recipe-card" onClick={onClick}>
+      <img src={recipe.image} alt="recipe.title"></img>
+      <div className="recipe-card-title">
+        <span
+          onClick={(event) => {
+            event.stopPropagation();
+            onFavouriteButtonClick(recipe);
+          }}
+        >
+          {isFavourite ? (
+            <AiFillHeart size={25} color="red" />
+          ) : (
+            <AiOutlineHeart size={25} />
+          )}
+        </span>
+        <h3>{recipe.title}</h3>
+      </div>
+    </div>
+  );
+};
 
 export default RecipeCard;
