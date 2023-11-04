c1dce964fff2e3a3d770a26845f2619f690075d9
krisnadharma1412 <iputukrisnadharma@gmail.com>
2023-11-04 22:22:20 +0700

    edit api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import { Recipe } from "./types";
 
 export const searchRecipes = async (searchTerm: string, page: number) => {
-    const baseUrl = new URL("http://localhost:5000/api/recipes/search");
+    const baseUrl = new URL("https://recipe-web-server.vercel.app/api/recipes/search");
     baseUrl.searchParams.append("searchTerm",searchTerm);
     baseUrl.searchParams.append("page", String(page));
 
@@ -14,7 +14,7 @@ export const searchRecipes = async (searchTerm: string, page: number) => {
 };
 
 export const getRecipeSummary = async (recipeId: string) => {
-    const url = new URL(`http://localhost:5000/api/recipes/${recipeId}/summary`)
+    const url = new URL(`https://recipe-web-server.vercel.app/api/recipes/${recipeId}/summary`)
     const response =  await fetch(url)
 
     if(!response.ok){
@@ -25,7 +25,7 @@ export const getRecipeSummary = async (recipeId: string) => {
 }
 
 export const getFavouriteRecipes = async() => {
-    const url = new URL('http://localhost:5000/api/recipes/favourite')
+    const url = new URL('https://recipe-web-server.vercel.app/api/recipes/favourite')
     const response = await fetch(url)
     
     if(!response.ok){
@@ -36,7 +36,7 @@ export const getFavouriteRecipes = async() => {
 }
 
 export const addFavouriteRecipe = async(recipe: Recipe) => {
-    const url = new URL('http://localhost:5000/api/recipes/favourite')
+    const url = new URL('https://recipe-web-server.vercel.app/api/recipes/favourite')
     const body = {
         recipeId: recipe.id
     }
@@ -55,7 +55,7 @@ export const addFavouriteRecipe = async(recipe: Recipe) => {
 }
 
 export const removeFavouriteRecipe = async (recipe: Recipe) => {
-    const url = new URL("http://localhost:5000/api/recipes/favourite");
+    const url = new URL("https://recipe-web-server.vercel.app/api/recipes/favourite");
     const body = {
         recipeId: recipe.id
     }
